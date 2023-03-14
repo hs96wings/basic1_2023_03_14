6d21ed0177780d648852f98d82814db2b49ef0ee
hs96wings <hs96wings@gmail.com>
2023-03-14 14:33:51 +0900

    Person class 추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,15 +1,27 @@
 package com.ll.basic1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RestController
 public class HomeController {
     private int num;
+    private List<Person> people = new ArrayList<>();
 
     public HomeController() {
         this.num = 0;
+        this.people = new ArrayList<>();
     }
 
     @GetMapping("/home/main")
@@ -30,9 +42,47 @@ public class HomeController {
         return "즐거웠습니다";
     }
 
-    @GetMapping("/increase")
+    @GetMapping("/home/increase")
     @ResponseBody
     public String showIncrease() {
         return "응답: " + num++;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(@RequestParam String name, int age) {
+        Person p = new Person(name, age);
+        people.add(p);
+        return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return people;
+    }
+}
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Person {
+    private static int lastId;
+    private final int id;
+    private final String name;
+    private final int age;
+
+    static {
+        lastId = 0;
+    }
+
+    Person(String name, int age) {
+        this(++lastId, name, age);
     }
 }
