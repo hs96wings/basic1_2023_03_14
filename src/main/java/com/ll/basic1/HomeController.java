bbac4dd97a82eed36c25c65f371a925c7a8f0570
hs96wings <hs96wings@gmail.com>
2023-03-14 11:44:07 +0900

    C:/Program Files/Git/increase

---
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -0,0 +1,38 @@
+package com.ll.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    private int num;
+
+    public HomeController() {
+        this.num = 0;
+    }
+
+    @GetMapping("/home/main")
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다";
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public String showIncrease() {
+        return "응답: " + num++;
+    }
+}
