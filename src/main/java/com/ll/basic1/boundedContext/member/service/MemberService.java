6f5f83f68206232205fc9b95ff11e778f10d63cc
hs96wings <hs96wings@gmail.com>
2023-03-15 13:13:44 +0900

    MemberRepository 도입

--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -1,14 +1,27 @@
 package com.ll.basic1.boundedContext.member.service;
 
 import com.ll.basic1.base.RsData;
+import com.ll.basic1.boundedContext.member.entity.Member;
+import com.ll.basic1.boundedContext.member.repository.MemberRepository;
 
 public class MemberService {
-    public RsData tryLogin(String username, String password) {
-        if (!password.equals("1234"))
-            return RsData.of("F-1", "비밀번호가 일치하지 않습니다");
-        else if (!username.equals("user1"))
-            return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다".formatted(username));
+    private MemberRepository memberRepository;
 
-        return RsData.of("S-1", "%d님 환영합니다".formatted(username));
+    public MemberService() {
+        memberRepository = new MemberRepository();
+    }
+
+    public RsData tryLogin(String username, String password) {
+        Member member = memberRepository.findByUsername(username);
+
+        if (member == null) {
+            return RsData.of("F-2", "%s(은)는 존재하지 않는 회원입니다");
+        }
+
+        if (!member.getPassword().equals(password)) {
+            return RsData.of("F-1", "비밀번호가 일치하지 않습니다");
+        }
+
+        return RsData.of("S-1", "%s 님 환영합니다".formatted(username));
     }
 }
